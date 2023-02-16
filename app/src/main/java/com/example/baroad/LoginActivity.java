1a79ba6f8905ee1d3a4c1f794dde92fa8d0c6a76
문채영 <s2123@e-mirim.hs.kr>
2023-02-16 11:15:30 +0900

    로그인

--- app/src/main/java/com/example/baroad/LoginActivity.java
+++ app/src/main/java/com/example/baroad/LoginActivity.java
@@ -1,16 +1,58 @@
 package com.example.baroad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        EditText IDText = findViewById(R.id.id_text);
+        EditText PassText = findViewById(R.id.pass_text);
+
+        findViewById(R.id.go_main_btn).setOnClickListener(v -> {
+            if(!IDText.getText().toString().equals("") && !PassText.getText().toString().equals("")) {
+                auth.signInWithEmailAndPassword(IDText.getText().toString(), PassText.getText().toString())
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, "로그인 완료!", Toast.LENGTH_SHORT).show();
+
+                                    auth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+                                        @Override
+                                        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                                            if(user != null) {
+                                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                                startActivity(intent);
+                                                finish();
+                                            }
+                                        }
+                                    });
+                                }
+                            }
+                        });
+            }
+            else {
+                Toast.makeText(LoginActivity.this, "계정과 비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         findViewById(R.id.go_sign_btn).setOnClickListener(v-> {
             Intent intent = new Intent(LoginActivity.this, SignInActivity.class);
