4b3cec0c0db02b19fa03a2440ae8408238f031dc
문채영 <s2123@e-mirim.hs.kr>
2023-02-17 02:49:27 +0900

    bottom navigation 추가

--- app/src/main/java/com/example/baroad/MainActivity.java
+++ app/src/main/java/com/example/baroad/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.baroad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottom = findViewById(R.id.navigationView);
+        getSupportFragmentManager().beginTransaction().add(R.id.frame, new MainFragment()).commit();
     }
+
 }
---
+++ app/src/main/java/com/example/baroad/MainFragment.java
@@ -0,0 +1,21 @@
+package com.example.baroad;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+
+
+    }
+}
