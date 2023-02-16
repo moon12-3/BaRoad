44ffcfd712434310f19cced2bb72a3233a31d572
문채영 <s2123@e-mirim.hs.kr>
2023-02-16 09:42:25 +0900

    회원가입

--- app/src/main/java/com/example/baroad/LoginActivity.java
+++ app/src/main/java/com/example/baroad/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.baroad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
@@ -10,5 +11,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.go_sign_btn).setOnClickListener(v-> {
+            Intent intent = new Intent(LoginActivity.this, SignInActivity.class);
+            startActivity(intent);
+        });
     }
+
+
 }
