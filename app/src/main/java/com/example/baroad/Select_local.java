efd8bcc8b12abf2f5c19184b000591792ceffb49
chaeyoung Moon <80873608+moon12-3@users.noreply.github.com>
2023-05-29 17:30:37 +0900

    이름 및 build설정 수정

--- app/src/main/java/com/example/baroad/Select_local.java
+++ app/src/main/java/com/example/baroad/Select_local.java
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
-import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
