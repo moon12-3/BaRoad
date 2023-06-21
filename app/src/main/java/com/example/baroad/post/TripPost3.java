bedb2a2c46e9cd9b77cbd9bf9c6a0f70bbffe288
chaeyoung Moon <80873608+moon12-3@users.noreply.github.com>
2023-06-22 07:08:24 +0900

    일정 추가 끝

--- app/src/main/java/com/example/baroad/post/TripPost3.java
+++ app/src/main/java/com/example/baroad/post/TripPost3.java
@@ -2,13 +2,32 @@ package com.example.baroad.post;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.baroad.Model.MapModel;
+import com.example.baroad.Model.PlanModel;
 import com.example.baroad.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.WriteBatch;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class TripPost3 extends Fragment {
 
@@ -24,6 +43,60 @@ public class TripPost3 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trip_post3, container, false);
+        View view = inflater.inflate(R.layout.fragment_trip_post3, container, false);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        // WriteBatch 인스턴스 생성
+        WriteBatch batch = db.batch();
+
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy. MM. dd");
+        String getTime = sdf.format(date);
+        PlanModel plan = new PlanModel(getTime, "나고야");
+
+        List<MapModel> docu = new ArrayList<>();
+        docu.add(new MapModel("나고야 역", "1 Chome-1-4 Meieki, Nakamura Ward, Nagoya, Aichi", 35.17091527561959, 136.88153439890988, "", "", 1L));
+        docu.add(new MapModel("히가시야마 동물원", "아이치현 나고야시 치쿠사구 히가시야마 모토마치 3쵸메-70", 35.15670040248381, 136.98102719494906, "", "", 2L));
+        docu.add(new MapModel("다이노 어드벤쳐 나고야", "나고야시 나카구 혼마루 1-1", 35.0659707823332, 136.95432099811353, "", "", 3L));
+        docu.add(new MapModel("나고야시 과학관", "나고야시 나카구 사카에 2쵸메 17번 1호", 35.165176862335656, 136.89969665363884, "", "", 4L));
+
+        view.findViewById(R.id.add_cos).setOnClickListener(v->{
+            db.collection("users").document(auth.getCurrentUser().getEmail())
+                    .collection("plan").add(plan)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            CollectionReference collectionRef = db.collection("users").document(auth.getCurrentUser().getEmail())
+                                    .collection("plan").document(documentReference.getId()).collection("maps");
+                            for(MapModel doc : docu) {
+                                DocumentReference docRef = collectionRef.document(); // 자동 생성된 문서 ID
+                                batch.set(docRef, doc);
+                            }
+                            batch.commit().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getActivity(), "내 일정에 추가되었습니다!", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Exception e = task.getException();
+                                        if (e != null) {
+                                            System.out.println("문서 추가 중 오류 발생: " + e.getMessage());
+                                        }
+                                    }
+                                }
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("mytag", "Error writing document", e);
+                        }
+                    });
+        });
+
+        return view;
     }
 }
--- app/src/main/java/com/example/baroad/post/TripPost4.java
+++ app/src/main/java/com/example/baroad/post/TripPost4.java
@@ -2,13 +2,32 @@ package com.example.baroad.post;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.baroad.Model.MapModel;
+import com.example.baroad.Model.PlanModel;
 import com.example.baroad.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.WriteBatch;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 
 public class TripPost4 extends Fragment {
@@ -24,6 +43,60 @@ public class TripPost4 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trip_post4, container, false);
+        View view = inflater.inflate(R.layout.fragment_trip_post4, container, false);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        // WriteBatch 인스턴스 생성
+        WriteBatch batch = db.batch();
+
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy. MM. dd");
+        String getTime = sdf.format(date);
+        PlanModel plan = new PlanModel(getTime, "나고야");
+
+        List<MapModel> docu = new ArrayList<>();
+        docu.add(new MapModel("나고야 역", "1 Chome-1-4 Meieki, Nakamura Ward, Nagoya, Aichi", 35.17091527561959, 136.88153439890988, "", "", 1L));
+        docu.add(new MapModel("크루즈 나고야", "2 Ungacho, Nakagawa Ward, Nagoya, Aichi 454-0001 일본", 35.16026168738063, 136.8839313181859, "", "", 2L));
+        docu.add(new MapModel("레고랜드", "아이치현 나고야시 미나토구 긴조후토 2-2-1", 35.05061525591104, 136.84382929691986, "", "", 3L));
+        docu.add(new MapModel("메이커스피어", "나고야시 미나토구 긴조후토 2-7-1", 35.05056566622372, 136.84711367116986, "", "", 4L));
+
+        view.findViewById(R.id.add_cos).setOnClickListener(v->{
+            db.collection("users").document(auth.getCurrentUser().getEmail())
+                    .collection("plan").add(plan)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            CollectionReference collectionRef = db.collection("users").document(auth.getCurrentUser().getEmail())
+                                    .collection("plan").document(documentReference.getId()).collection("maps");
+                            for(MapModel doc : docu) {
+                                DocumentReference docRef = collectionRef.document(); // 자동 생성된 문서 ID
+                                batch.set(docRef, doc);
+                            }
+                            batch.commit().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getActivity(), "내 일정에 추가되었습니다!", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Exception e = task.getException();
+                                        if (e != null) {
+                                            System.out.println("문서 추가 중 오류 발생: " + e.getMessage());
+                                        }
+                                    }
+                                }
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("mytag", "Error writing document", e);
+                        }
+                    });
+        });
+
+        return view;
     }
 }
